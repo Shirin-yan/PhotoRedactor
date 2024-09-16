986e49c042189946b15e8bd453cd5c220a39c519
Shirin-Yan <shirin-yan@ShirinMBP.local>
2024-09-16 19:21:12 +0500

    Added project

---
+++ PhotoRedactor/PhotoRedactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoRedactorApp.swift
+//  PhotoRedactor
+//
+//  Created by Shirin-Yan on 16.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoRedactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
